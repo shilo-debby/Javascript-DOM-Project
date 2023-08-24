da4eb25076272501b98b5f1a58b04f41b1ab906c
shilo-debby <gdeborahshilo@gmail.com>
2023-08-24 17:16:30 +0200

    query selector

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
-const titles=document.getElementsByClassName('title');
+// const titles=document.getElementsByClassName('title');
 
-Array.from(titles).forEach(function(item){
-    console.log(item.innerHTML)
+// Array.from(titles).forEach(function(item){
+//     console.log(item.innerHTML)
+// })
+
+// const wrap = document.querySelector('#book-list ul li:nth-child(2) .name');
+// console.log(wrap.innerHTML);
+
+// const firstList= document.querySelector('#book-list ul li');
+// console.log(firstList);
+
+
+
+const books=document.querySelectorAll('#book-list ul li .name');
+Array.from(books).forEach(function(title){
+    console.log(title.innerHTML);
 })
