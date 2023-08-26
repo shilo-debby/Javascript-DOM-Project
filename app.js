4418acf10fd805685e29b2ead2bf5edc46e9aca7
shilo-debby <gdeborahshilo@gmail.com>
2023-08-26 12:08:22 +0200

    changing text and html

--- app.js
+++ app.js
@@ -16,3 +16,12 @@ const books=document.querySelectorAll('#book-list ul li .name');
 Array.from(books).forEach(function(title){
     console.log(title.innerHTML);
 })
+Array.from(books).forEach(function(book){
+    book.textContent+='(booktitle)';
+});
+const booklist=document.querySelector('#book-list');
+booklist.innerHTML='<h2>Books and more books...</h2>';
+booklist.innerHTML+='<p>This is how you add HTML</p>';
+console.log(booklist)
+
+
