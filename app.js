0cfaaa7d2b304032101655fd27d77ab233d952c4
shilo-debby <gdeborahshilo@gmail.com>
2023-08-26 17:30:40 +0200

    adding book events

--- app.js
+++ app.js
@@ -6,3 +6,23 @@ list.addEventListener('click',function(e){
         li.parentNode.removeChild(li);
     }
 })
+
+// Adding a book
+const addForm=document.forms['add-book'];
+
+addForm.addEventListener('submit',function(e) {
+    e.preventDefault();
+    const value = document.querySelector('#add-book input').value;
+    // create elements
+    const li =document.createElement('li');
+    const bookName=document.createElement('span');
+    const deleteBtn=document.createElement('span');
+    //giving value to the elements
+    bookName.textContent=value;
+    deleteBtn.textContent='delete';
+    // appending child
+    li.appendChild(bookName);
+    li.appendChild(deleteBtn);
+    list.appendChild(li);
+})
+
