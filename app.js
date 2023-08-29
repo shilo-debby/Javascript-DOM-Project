2192aa9628bacc8f47c4ff037740aae05c330deb
shilo-debby <gdeborahshilo@gmail.com>
2023-08-29 11:08:39 +0200

    checkboxes and change events

--- app.js
+++ app.js
@@ -1,34 +1,51 @@
 // Detele buttons events
-const list= document.querySelector('#book-list ul');
-list.addEventListener('click',function(e){
-    if(e.target.className=='delete') {
-        const li = e.target.parentNode;
-        li.parentNode.removeChild(li);
-    }
+// const list= document.querySelector('#book-list ul');
+// list.addEventListener('click',function(e){
+//     if(e.target.className=='delete') {
+//         const li = e.target.parentNode;
+//         li.parentNode.removeChild(li);
+//     }
+// })
+
+// // Adding a book
+// const addForm=document.forms['add-book'];
+
+// addForm.addEventListener('submit',function(e) {
+//     e.preventDefault();
+//     const value = document.querySelector('#add-book input').value;
+//     // create elements
+//     const li =document.createElement('li');
+//     const bookName=document.createElement('span');
+//     const deleteBtn=document.createElement('span');
+//     //giving value to the elements
+//     bookName.textContent=value;
+//     deleteBtn.textContent='delete';
+//     // adding classes and style
+//     bookName.classList.add('name');
+//     deleteBtn.classList.add('delete')
+//     // appending child
+//     li.appendChild(bookName);
+//     li.appendChild(deleteBtn);
+//     list.appendChild(li);
+
+// })
+
+
+// //Attibutes
+// var book=document.querySelector('li :first-child.name')
+// book.getAttributes('class')
+// book.getAttribute('class','names-1')
+// book.hasAttributes('class')
+// book.setAttributes(' ','')
+
+//checkboxes and change events
+const hideBox=document.querySelector('#hide');
+hideBox.addEventListener('change',function(e){
+if(hideBox.checked){
+list.style.display  ="none"
+}else{
+    list.style.display="initial"
+}
 })
 
-// Adding a book
-const addForm=document.forms['add-book'];
-
-addForm.addEventListener('submit',function(e) {
-    e.preventDefault();
-    const value = document.querySelector('#add-book input').value;
-    // create elements
-    const li =document.createElement('li');
-    const bookName=document.createElement('span');
-    const deleteBtn=document.createElement('span');
-    //giving value to the elements
-    bookName.textContent=value;
-    deleteBtn.textContent='delete';
-    // adding classes and style
-    bookName.classList.add('name');
-    deleteBtn.classList.add('delete')
-    // appending child
-    li.appendChild(bookName);
-    li.appendChild(deleteBtn);
-    list.appendChild(li);
-
-})
-
-
-
+//
